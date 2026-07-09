d3b0608fb3f490314129517b467062cee14bb5c8
RANOARISON Hasiniaina Hardy <Hasinahardy2202gmail.com>
2026-07-09 09:11:57 +0300

    Client.py

--- Client.py
+++ Client.py
@@ -1,15 +1,71 @@
 import socket
-from tkinter import Tk, filedialog
+from tkinter import Tk, filedialog, simpledialog
 import os
+import hashlib
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     client.connect(("192.168.88.36", 5000))
-    print("Connecté au serveur.")
+    print("Connecte au serveur.")
 except ConnectionRefusedError:
     print("Impossible de se connecter : le serveur n'est pas demarre.")
     exit()
+# ---------------- Login ----------------
+root = Tk()
+root.withdraw()
+
+utilisateur = simpledialog.askstring(
+    "Connexion",
+    "Nom utilisateur :"
+)
+
+mot_de_passe = simpledialog.askstring(
+    "Connexion",
+    "Mot de passe :",
+    show="*"
+)
+
+root.destroy()
+
+
+if utilisateur is None or mot_de_passe is None:
+    print("Connexion annule.")
+    client.close()
+    exit()
+
+
+client.send(
+    f"LOGIN|{utilisateur}|{mot_de_passe}\n".encode()
+)
+reponse = client.recv(4096).decode()
+
+print(reponse)
+
+if not reponse.startswith("OK"):
+    print("Connexion refuse.")
+    client.close()
+    exit()
+
+print("Connexion reussi.")
+
+def calculer_hash(chemin):
+
+    sha256 = hashlib.sha256()
+
+    with open(chemin, "rb") as f:
+
+        while True:
+
+            data = f.read(4096)
+
+            if not data:
+                break
+
+            sha256.update(data)
+
+    return sha256.hexdigest()
 
 while True:
     print("\n -Menu- ")
@@ -37,6 +93,7 @@ while True:
         else:
             print(reponse)
 
+
     elif choix == "2":
 
         root = Tk()
@@ -58,7 +115,9 @@ while True:
 
         client.send(f"UPLOAD|{nom}|{taille}\n".encode())
 
+
         reponse = client.recv(4096).decode()
+
         print(reponse)
 
         if not reponse.startswith("OK"):
@@ -66,23 +125,28 @@ while True:
             continue
 
         with open(chemin, "rb") as f:
+
             while True:
+
                 data = f.read(4096)
 
                 if not data:
                     break
 
                 client.sendall(data)
-        
+                
         reponse = client.recv(4096).decode()
+
         print(reponse)
 
         print("Fichier envoye :", nom)
 
     elif choix == "3":
+
         nom = input("Nom du fichier a telecharger : ")
 
         client.send(f"DOWNLOAD|{nom}\n".encode())
+
         reponse = client.recv(4096).decode()
 
         if reponse.startswith("ERROR"):
@@ -94,12 +158,14 @@ while True:
         if parties[0] == "FILE":
 
             taille = int(parties[2])
+            hash_attendu = parties[3]
+
             recu = 0
 
             with open(nom, "wb") as f:
 
                 while recu < taille:
-
+                    
                     data = client.recv(
                         min(4096, taille - recu)
                     )
@@ -108,15 +174,31 @@ while True:
                         break
 
                     f.write(data)
+
                     recu += len(data)
 
             print("Telechargement termine.")
+
             print("Taille reçue :", recu, "octets")
+
+            hash_local = calculer_hash(nom)
+            if hash_local == hash_attendu:
+
+                print("Verification integrite reussi.")
+
+            else:
+
+                print("Erreur : fichier corrompu.")
     elif choix == "4":
+
         client.send("Quitter".encode())
+
         client.close()
+
         print("Connexion fermee.")
+
         break
 
     else:
+
         print("Choix invalide.")
